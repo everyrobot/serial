2b82f8012c5376707dd5c82f8643ef1e956019b1
HasanFarag <53587583+HasanFarag@users.noreply.github.com>
2019-12-12 10:00:55 +0100

    Update ros_ws_install.sh

--- ros_ws_install.sh
+++ ros_ws_install.sh
@@ -3,12 +3,8 @@ mkdir serial_control_ws
 cd serial_control_ws 
 mkdir src   
 cd src 
-git clone https://github.com/everyrobot/er_ti_f28069m_drv8305.git
-git clone https://github.com/everyrobot/serial-example.git
-cd serial-example
-git fetch
-git checkout -b linux_serial
-git pull origin linux_serial
+git clone https://github.com/everyrobot/serial-ros.git
+git clone --branch er_drv_ros https://github.com/everyrobot/serial-ros.git
 cd ../.. 
 catkin_make
 source devel/setup.bash
